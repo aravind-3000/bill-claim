2500155b742326a83d5f04f9833d06763a1cb0db
aravind-3000 <aravindraj2848@gmail.com>
2024-07-28 17:32:29 +0530

    created department curd

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_27_125411) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_28_115805) do
+  create_table "departments", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
